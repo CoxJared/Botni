d2ddfbb8caeb801391beb33f97febb5df76e61f0
Jared Cox <coxj1@mcmaster.ca>
2020-07-26 20:42:19 -0400

    login request

--- functions/index.js
+++ functions/index.js
@@ -63,6 +63,23 @@ app.post('/scream', (request, response) => {
     });
 });
 
+const isEmail = (email) => {
+  const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  if (email.match(regEx)) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+const isEmpty = function(string) {
+  if (string.trim() === '') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
 //signup route
 app.post('/signup', (request, response) => {
   const newUser = {
@@ -72,21 +89,55 @@ app.post('/signup', (request, response) => {
     handle: request.body.handle
   };
 
+  let errors = {};
+
+  if (isEmpty(newUser.email)) {
+    errors.email = 'Must not be empty';
+  } else if (!isEmail(newUser.email)) {
+    errors.email = 'Must be a valid email address';
+  }
+  if (isEmpty(newUser.password)) {
+    errors.password = 'Must not be empty';
+  }
+  if (newUser.password != newUser.confirmPassword) {
+    errors.confirmPassword = `Passwords must match`;
+  }
+  if (isEmpty(newUser.handle)) {
+    errors.handle = 'Must not be empty';
+  }
+
+  if (Object.keys(errors).length > 0) {
+    return response.status(400).json({ errors });
+  }
+
   //validate data
-  db.doc(`/users/${newUser.handle}`).get()
-    .then(doc => {
-      if(doc.exists) {
-        return response.status(400).json({ handle: 'this handle is already taken'});
+  let token, userId;
+  db.doc(`/users/${newUser.handle}`)
+    .get()
+    .then((doc) => {
+      if (doc.exists) {
+        return response.status(400).json({ handle: 'this handle is already taken' });
       } else {
         return firebase
           .auth()
           .createUserWithEmailAndPassword(newUser.email, newUser.password);
       }
     })
-    .then(data => {
+    .then((data) => {
+      userId = data.user.uid;
       return data.user.getIdToken();
     })
-    .then(token => {
+    .then((idToken) => {
+      token = idToken; 
+      const userCredentials = {
+        handle: newUser.handle,
+        email: newUser.email,
+        createdAt: new Date().toISOString(),
+        userId
+      };
+      return db.doc(`/users/${newUser.handle}`).set(userCredentials);
+    })
+    .then(() => {
       return response.status(201).json({ token });
     })
     .catch(err => {
@@ -99,7 +150,38 @@ app.post('/signup', (request, response) => {
     })
 })
 
+app.post('/login', (request, response) => {
+  const user = {
+    email: request.body.email,
+    password: request.body.password
+  };
 
+  let errors = {};
+
+  if (isEmpty(user.email)) {
+    errors.email = 'Must not be empty';
+  }
+  if (isEmpty(user.password)) {
+    errors.password = 'Must not be empty';
+  }
+  if (Object.keys(errors).length > 0) {
+    return response.status(400).json({ errors });
+  }
+
+  firebase
+    .auth()
+    .signInWithEmailAndPassword(user.email, user.password)
+    .then((data) => {
+      return data.user.getIdToken();
+    })
+    .then((idToken) => {
+      return response.json({ token: idToken });
+    })
+    .catch((err) => {
+      console.err(err);
+      return response.status(500).json({ error:error.code });
+    })
+})
 
 exports.api = functions.https.onRequest(app);
 
